cedade4b6799fc39e95c3a8e40348fbd74b00e31
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-01 14:02:24 -0600

    Feelin real foolish on this whole thing, but moving on to It3

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -7,8 +7,13 @@ class Restaurant
     @dishes = []
   end
 
+  def closing_time(time)
+    (@opening_time.to_i + time).to_s + ':00' #looked this up via the answers accidentally given and then on apidock rubyguides.com because holy smokes I would never have thought to do this, but it makes so much sense.  I'd never seen or heard of this type of thing.  Take the "opening_time" string, turn it into an integer, add the time, convert back into a string, and add the ":00" back as it would have been deleted when "opening_time" was first turned into an integer"
+  end
 
-
+  def add_dish(dish)
+    @dishes << dish  #this one we learned earlier today or yesterday (with Kat either way), and I'm glad to see the answers matched me!
+  end
 
 
 
